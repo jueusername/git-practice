e4a02cc988540662103872d25d47a0149b5743ad
jueusername <127116473+jueusername@users.noreply.github.com>
2023-06-30 16:44:50 +0800

    Update githubTest.java
    hello github

--- src/githubTest.java
+++ src/githubTest.java
@@ -2,6 +2,7 @@ public class githubTest {
     public static void main(String[] args) {
         System.out.println("github Test");
          System.out.println("github pull Test");
+        System.out.println("hello github");
     
     }
 }
